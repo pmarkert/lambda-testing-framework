4d2c43739b87af7889ae79df1a325817f414a020
Phillip Markert <phillip@ephisys.com>
2016-11-30 22:57:08 -0800

    Initial commit

--- index.js
+++ index.js
@@ -1,49 +1,40 @@
-const context = require('aws-lambda-mock-context');
+const mock_context = require('aws-lambda-mock-context');
 const async = require("async");
 const chai = require("chai").should();
 const path = require("path");
 const fs = require("fs");
 global.TEST_MODE = true;
 
-module.exports = function(method_under_test) {
-	describe("Event processing:", function(done) {
-		async.each(fs.readdirSync("./test/events"), function (dirname, dir_cb) {
-			if(fs.lstatSync(path.join("./test/events", dirname)).isDirectory()) {
-				describe("State:" + dirname, function() {
-					async.each(fs.readdirSync(path.resolve(__dirname, path.join("events", dirname))), function(filename) {
-						if(!filename.endsWith(".response.json")) {
-							it("Case:" + filename, function (event_done) {
-								const file_content = require("./" + path.join("events", dirname, filename));
-								const ctx = context();
-								method_under_test(file_content, ctx);
-								ctx.Promise.then(response => {
-									try {
-										const response_filename = path.join("events", dirname, filename.substring(0,filename.length - 5) + ".response.json");
-										if(!fs.existsSync(path.resolve(__dirname, response_filename))) {
-											if(process.env.SAVE_RESPONSES) {
-												fs.writeFileSync(path.resolve(__dirname, response_filename), JSON.stringify(response, null, 2));
-												console.log(`Response for ${response_filename} saved.`);
-											} else {
-												console.log(`Response for ${response_filename} does not exist:`);
-												console.log(JSON.stringify(response, null, 2));
-											}
-											return event_done(`Response file ${response_filename} does not exist yet.`);
-										}
-										const expected_response = require("./" + response_filename);
-										response.should.deep.equal(expected_response);			
-										return event_done();
-									}
-									catch(err) {
-										return event_done(err);
-									}
-								})
-								.catch(event_done);
-							});
+module.exports = function(method_under_test, path_to_tests, done) {
+	async.each(fs.readdirSync(path_to_tests), function(filename) {
+		if(filename.endsWith(".json") && !filename.endsWith(".response.json")) {
+			it("case:" + filename, function (testcase_done) {
+				const file_content = JSON.parse(fs.readFileSync(path.join(path_to_tests, filename)));
+				const context = mock_context();
+				method_under_test(file_content, context);
+				context.Promise.then(response => {
+					try {
+						const response_filename = path.join(path_to_tests, filename.substring(0,filename.lastIndexOf(".")) + ".response.json");
+						if(!fs.existsSync( response_filename)) {
+							if(process.env.SAVE_RESPONSES) {
+								fs.writeFileSync(response_filename, JSON.stringify(response, null, 2));
+								console.log(`Response for ${response_filename} saved.`);
+							} else {
+								console.log(`Response for ${response_filename} does not exist:`);
+								console.log(JSON.stringify(response, null, 2));
+							}
+							return testcase_done(`Response file ${response_filename} does not exist yet.`);
 						}
-					}, dir_cb);
-				});
-			}
-			else { dir_cb(); }
-		}, done);
-	});
+						const expected_response = JSON.parse(fs.readFileSync(response_filename));
+						response.should.deep.equal(expected_response);			
+						return testcase_done();
+					}
+					catch(err) {
+						return testcase_done(err);
+					}
+				})
+				.catch(testcase_done);
+			});
+		}
+	}, done);
 }
