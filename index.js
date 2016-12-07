0f9731e67bcd8dea8efe0f8da2e8c5c4e438ace6
Phillip Markert <phillip@ephisys.com>
2016-12-06 21:01:22 -0500

    Handling for failed expectations as well

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 const mock_context = require('aws-lambda-mock-context');
 const async = require("async");
-const chai = require("chai").should();
 const path = require("path");
 const fs = require("fs");
+const chai = require("chai").should();
 global.TEST_MODE = true;
 
-module.exports = function(method_under_test, path_to_tests, done) {
+module.exports = function(method_under_test, path_to_tests, done, expect_failure) {
 	async.each(fs.readdirSync(path_to_tests), function(filename) {
 		if(filename.endsWith(".request.json")) {
 			it(filename, function (testcase_done) {
+				const testcase_filename = path.join(path_to_tests, filename);
 				const file_content = JSON.parse(fs.readFileSync(path.join(path_to_tests, filename)));
 				const context = mock_context();
 				method_under_test(file_content, context);
 				context.Promise.then(response => {
-					try {
-						const response_filename = path.join(path_to_tests, filename.replace(/\.request\.json$/,".response.json"));
-						if(!fs.existsSync( response_filename)) {
-							if(process.env.SAVE_RESPONSES) {
-								fs.writeFileSync(response_filename, JSON.stringify(response, null, 2));
-								console.log(`Response for ${response_filename} saved.`);
-							} else {
-								console.log(`Response for ${response_filename} does not exist:`);
-								console.log(JSON.stringify(response, null, 2));
-							}
-							return testcase_done(`Response file ${response_filename} does not exist yet.`);
-						}
-						const expected_response = JSON.parse(fs.readFileSync(response_filename));
-						response.should.deep.equal(expected_response);			
-						return testcase_done();
+					if(expect_failure) {
+						return testcase_done("Test case should have failed, but instead succeeded - " + JSON.stringify(response, null, 2)); 
 					}
-					catch(err) {
+					matches_expected_response(response, testcase_filename, testcase_done);
+				})
+				.catch(err => {
+					if(!expect_failure) {
 						return testcase_done(err);
 					}
-				})
-				.catch(testcase_done);
+					matches_expected_response(err.message, testcase_filename, testcase_done);
+				});
 			});
 		}
 	}, done);
+}
+
+function matches_expected_response(response, testcase_filename, testcase_done) {
+	try {
+		var expected_response;
+		var response_filename = testcase_filename.replace(/\.request\.json$/,".response.pattern");
+		if(!fs.existsSync(response_filename)) {
+			response_filename = testcase_filename.replace(/\.request\.json$/,".response.json")
+			if(!fs.existsSync(response_filename)) {
+				if(process.env.SAVE_RESPONSES) {
+					fs.writeFileSync(response_filename, response);
+					console.log(`Response for ${response_filename} saved.`);
+				} else {
+					console.log(`Response for ${response_filename} does not exist:`);
+					console.log(JSON.stringify(response));
+				}
+				return testcase_done(`Response file ${response_filename} does not exist yet.`);
+			}
+			else {
+				expected_response = JSON.parse(fs.readFileSync(response_filename));
+				response.should.deep.equals(expected_response);
+				testcase_done();
+			}
+		}
+		else {
+			response = response.stack ? response.toString() : JSON.stringify(response, null, 2);
+			expected_response = fs.readFileSync(response_filename, 'utf-8').trim();
+			if(response.match(expected_response)) {
+				return testcase_done();
+			}
+			else {
+				return testcase_done("Response did not match expected pattern - " + response);
+			}
+			
+		}
+	} catch(err) {
+		testcase_done(err);
+	}
 }
