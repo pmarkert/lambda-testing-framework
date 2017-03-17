19ae191acc55c7d25ec5a7f869b34a9639857653
Phillip Markert <phillip@ephisys.com>
2017-03-17 17:19:25 -0400

    Updated injectable.js to handle paths better

--- injectable.js
+++ injectable.js
@@ -1,16 +1,23 @@
 var real_require = require;
 var path = require("path");
 
-module.exports = function(base_path, relative_path) {
-	if(relative_path) base_path = path.join(base_path, relative_path);
+module.exports = function(requirer_path, mocks_path) {
+	var mocks_path = path.join(requirer_path, mocks_path);
 	return {
 		require: function require(module_name, instance) {
 			if(global.TEST_MODE) {
-				return real_require(path.join(base_path, module_name));
+				return real_require(path.join(mocks_path, module_name));
 			}
 			else {
-				return instance || real_require(module_name);
+				return instance || real_require(adjust_module_path(module_name));
 			}
 		}
+	}
+
+	function adjust_module_path(module_name) {
+		if(!module_name.startsWith(".")) {
+			return module_name;
+		}
+		return path.join(requirer_path, module_name);
 	}
 }
